cceb163dea79adbcdfc79745c72ead34f0a19fee
mmeyer2k <m.meyer2k@gmail.com>
2019-07-12 00:47:23 -0500

    no message

--- UserInterface/Properties/AssemblyInfo.cs
+++ UserInterface/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cuttlefish")]
+[assembly: AssemblyTitle("drawbridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cuttlefish")]
+[assembly: AssemblyProduct("drawbridge")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
